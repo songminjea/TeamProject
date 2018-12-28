2b5fde4f5cdfeaec75c14e86756dabacdc0d2833
itwill <itwill@ITWILLBS04-04>
2018-12-28 12:29:36 +0900

    follower following List

--- src/main/java/com/team/gallery/Service/GBServiceImpl.java
+++ src/main/java/com/team/gallery/Service/GBServiceImpl.java
@@ -1,9 +1,11 @@
 package com.team.gallery.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.team.gallery.VO.galleryVO;
 
+@Service
 public class GBServiceImpl implements GBService {
 
 	@Override
